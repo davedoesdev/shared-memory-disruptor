c9ef2fd5250c472a5ccc2c7667c97af120609257
David Halls <dahalls@gmail.com>
2017-08-28 10:28:04 +0100

    Add empty Produce and Consume methods

--- src/disruptor.cc
+++ src/disruptor.cc
@@ -11,6 +11,8 @@
 #include <memory>
 #include <napi.h>
 
+typedef uint64_t sequence_t;
+
 class Disruptor : public Napi::ObjectWrap<Disruptor>
 {
 public:
@@ -22,11 +24,27 @@ public:
     // Unmap the shared memory. Don't access it again from this process!
     Napi::Value Release(const Napi::CallbackInfo& info);
 
+    // Return unconsumed values for a consumer
+    Napi::Value Consume(const Napi::CallbackInfo& info); 
+
+    // Produce a value
+    Napi::Value Produce(const Napi::CallbackInfo& info);
+
 private:
     int Release();
 
+    uint32_t num_elements;
+    uint32_t element_size;
+    uint32_t num_consumers;
+    uint32_t consumer;
+
     size_t shm_size;
     void* shm_buf;
+    
+    sequence_t *consumers;
+    sequence_t *cursor;
+    sequence_t *next;
+    void* elements;
 };
 
 struct CloseFD
@@ -55,10 +73,11 @@ Disruptor::Disruptor(const Napi::CallbackInfo& info) :
 {
     // Arguments
     Napi::String shm_name = info[0].As<Napi::String>();
-    uint32_t num_elements = info[1].As<Napi::Number>();
-    uint32_t element_size = info[2].As<Napi::Number>();
-    uint32_t num_consumers = info[3].As<Napi::Number>();
+    num_elements = info[1].As<Napi::Number>();
+    element_size = info[2].As<Napi::Number>();
+    num_consumers = info[3].As<Napi::Number>();
     bool init = info[4].As<Napi::Boolean>();
+    consumer = info[5].As<Napi::Number>();
 
     // Open shared memory object
     std::unique_ptr<int, CloseFD> shm_fd(new int(
@@ -74,7 +93,7 @@ Disruptor::Disruptor(const Napi::CallbackInfo& info) :
     // a sequence number for each consumer,
     // the cursor sequence number (last filled slot) and
     // the next sequence number (first free slot).
-    shm_size = (num_consumers + 2) * sizeof(uint64_t) +
+    shm_size = (num_consumers + 2) * sizeof(sequence_t) +
                num_elements * element_size;
 
     // Resize the shared memory if we're initializing it.
@@ -94,6 +113,11 @@ Disruptor::Disruptor(const Napi::CallbackInfo& info) :
     {
         ThrowErrnoError(info, "Failed to map shared memory");
     }
+
+    consumers = static_cast<sequence_t*>(shm_buf);
+    cursor = &consumers[num_consumers];
+    next = &cursor[1];
+    elements = &next[1];
 }
 
 Disruptor::~Disruptor()
@@ -128,11 +152,30 @@ Napi::Value Disruptor::Release(const Napi::CallbackInfo& info)
     return info.Env().Undefined();
 }
 
+Napi::Value Disruptor::Consume(const Napi::CallbackInfo& info)
+{
+    // Return all elements [&consumers[consumer], cursor]
+    // Need to use atomic ops
+
+    return info.Env().Undefined();
+}
+
+Napi::Value Disruptor::Produce(const Napi::CallbackInfo& info)
+{
+    // Check there is space (there isn't a consumer at next)
+
+    // Claim slot, write to it and then commit, waiting for the cursor
+
+    return info.Env().Undefined();
+}
+
 void Disruptor::Initialize(Napi::Env env, Napi::Object exports)
 {
     exports["Disruptor"] = DefineClass(env, "Disruptor",
     {
-        InstanceMethod("release", &Disruptor::Release)
+        InstanceMethod("release", &Disruptor::Release),
+        InstanceMethod("consume", &Disruptor::Consume),
+        InstanceMethod("produce", &Disruptor::Produce)
     });
 }
 
