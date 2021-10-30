d4eebf44750d4aee97223b4c348be9daf6420bee
David Halls <dahalls@gmail.com>
2021-10-30 09:19:04 +0100

    Use master documentation to avoid stale deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -59,11 +59,7 @@ module.exports = function (grunt)
             },
 
             documentation: {
-                cmd: './node_modules/.bin/documentation build -f html -o docs docs.js'
-            },
-
-            serve_documentation: {
-                cmd: './node_modules/.bin/documentation serve -w docs.js'
+                cmd: 'npx documentation build -f html -o docs docs.js'
             }
         }
     })
@@ -83,5 +79,4 @@ module.exports = function (grunt)
                                     'exec:cover_report',
                                     'exec:cover_check']);
     grunt.registerTask('docs', 'exec:documentation');
-    grunt.registerTask('serve_docs', 'exec:serve_documentation');
 };
